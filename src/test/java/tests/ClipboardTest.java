69eafc2fbba4be54d4f4ad6bad56a96372edb130
danvu <danvu@ya.ru>
2021-09-26 00:44:05 +0300

    added lambda changes

--- src/test/java/tests/ClipboardTest.java
+++ src/test/java/tests/ClipboardTest.java
@@ -11,10 +11,12 @@ import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class ClipboardTest extends TestBase {
+public class ClipboardTest {
     @Test
-      @DisabledIfSystemProperty(named = "selenide.remote", matches = "http.*", disabledReason = "Clipboard not " +
+      /*@DisabledIfSystemProperty(named = "selenide.remote", matches = "http.*", disabledReason = "Clipboard not " +
      "support on Selenium Grid yet")
+
+      */
     @Description("Soon to be implemented by me (or QA engineers)")
     @DisplayName("Проверяем копипасту из буфера обмена в поле ввода телефона на страницу личного кабинета")
     void clipboardLoadTest() {
